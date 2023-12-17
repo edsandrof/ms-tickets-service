1e6225c2183a787a5b92573e37bc6198fc944d73
Edsandro Freitas <edsandrofreitas@gmail.com>
2023-12-17 19:52:55 -0300

    starting project

---
+++ src/test/java/com/github/edsandrof/ticketsservice/TicketsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.edsandrof.ticketsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
